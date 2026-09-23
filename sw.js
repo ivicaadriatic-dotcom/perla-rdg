071417e6d401cb6ef54e8f58b3a721aff490c005
ivicaadriatic-dotcom <ivica.adriatic@gmail.com>
2026-09-23 14:33:48 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* PERLA NOTTE RDG — service worker
    Aplikacija radi i bez interneta; podaci ostaju u localStorage preglednika. */
-var VERZIJA = 'pn-rdg-v138';
+var VERZIJA = 'pn-rdg-v139';
 
 var JEZGRA = [
   './',
